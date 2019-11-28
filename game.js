525e84b76ccaa269362d54076068db4997304e5a
DAVID GRAHAM (1911734) <1911734@rgu.ac.uk>
2019-11-28 15:04:52 +0000

    Changed the game.

--- game.js
+++ game.js
@@ -1,195 +1,192 @@
-// Object destructuring to avoid writing
-// new five.(...) every time.
-const {Board, LCD, Button} = require('johnny-five');
-const wait = require('wait-for-stuff');
+// Modules
+const Five = require('johnny-five');
+const Oled = require('oled-js');
+const Font = require('oled-font-5x7');
+const {EventEmitter} = require('events');
+const play = require('./modules/play.js');
 
-const board = new Board({port: 'COM3'});
+var gameEnd = false;
 
-/*
+// Instances
+const board = new Five.Board({port: "COM4"});
+const emitter = new EventEmitter();
 
-Globals
-———————
-screen - represents the LCD.
-died   - global flag for the game loop. When true, game is over.
-player - the co-ordinates of the player character.
-obstacleChars - characters for the obstacles which will be picked from by the generateObstacle function.
+// Sprites
+const pikachu = require('./sprite/pikachu.js');
 
-*/
+// Sounds
+const anthem = require('./sounds/anthem.js');
 
-// Global components
-var screen, buttons;
+// Arduino Components
+var screens, buttons, piezo;
 
-global.died = false;
+// Cursors Corresponding to Options
+var currCursor = 0,
+    currOption = 'attack',
+    optionCursors = [
+        [0,0],
+        [0,9],
+        [1,0]
+    ],
+    optionNames = [ 'attack', 'bag', 'run' ];
 
-var isJumping = false;
-const player = [1,3];
-const obstacleChars = ['duck', 'pointerleft', 'bigpointerleft'];
+const actions = {
+    attack: () => {
+        screens['opponent'].clearDisplay();
+        screens['opponent'].setCursor(16, 28);
+        screens['opponent'].writeString(Font, 1, 'Bang, yer deid.', 1, true, 2);
+        screens['opponent'].update();
+    },
+    // Bag
+    bag: () => {
+        screens['opponent'].clearDisplay();
+        screens['opponent'].setCursor(4, 4);
+        screens['opponent'].writeString(Font, 1, 'Bag:', 1, true, 2);
+        
+        screens['opponent'].setCursor(4, 12);
+        screens['opponent'].writeString(Font, 1, 'Nuts, Medkit, Rolls', 1, true, 2);
+        
+        screens['opponent'].update();
+    },
+    // Run
+    run: () => {
+        gameEnd = true;
+
+        screens['opponent'].clearDisplay();
+        screens['opponent'].setCursor(16, 28);
+        screens['opponent'].writeString(Font, 1, 'You ran away.', 1, true, 2);
+        screens['opponent'].update();
+
+        emitter.emit('game-over');
+    }
+}
 
 board.on('ready', () => {
-    
-    let is_setup = false;
+
+    let is_set_up = false;
 
     try {
-        // Dynamically instantiate buttons.
         buttons = {
-            jump:    new Button({ pin: 5, isPullup: true}),
-            restart: new Button({ pin: 4, isPullup: true}),
+            action: new Five.Button({
+                pin: 2,
+                isPullup: true
+            }),
+            cycle: new Five.Button({
+                pin: 3,
+                isPullup: true
+            })
         }
 
-        // Dynamically assigns event handlers.
         for(let btn in buttons){
-            // btn is the name of the button (e.g., 'jump').
-            // buttons[btn] grabs the actual button object.
-            
-            // actions[btn] will assign the action function
-            // corresponding with the button's name.
-            buttons[btn].on('press', actions[btn]);
+            buttons[btn]._name = btn;
+            buttons[btn].on('press', function(){
+                if(!gameEnd){
+                    emitter.emit(btn);
+                }
+            });
         }
-        
-        screen = new LCD({
-            pins: [7, 8, 9, 10, 11, 12],
-            backlight: 6,
-            rows: 2,
-            cols: 16
-        });
-        
-        // Set the player characters.
-        screen.useChar('runninga'); // Jump
-        screen.useChar('runningb'); // Idle
-        
-        // Set the obstacle characters.
-        screen.useChar('duck');
-        screen.useChar('pointerleft');
-        screen.useChar('bigpointerleft');
 
-        // Let the program know
-        // that all's well.
-        is_setup = true;
+        piezo = new Five.Piezo(5);
+
+        screens = {
+            action: new Five.LCD({
+                pins: [7, 8, 9, 10, 11, 12],
+                backlight: 13,
+                rows: 2,
+                cols: 16
+            }),
+            opponent: new Oled(board, Five, {
+                width: 128,
+                height: 64,
+                address: 0x3C
+            })
+        }
+
+        screens['opponent'].clearDisplay();
+        screens['opponent'].setCursor(4, 24);
+        screens['opponent'].writeString(Font, 1, 'Press the button to begin.', 1, true, 2);
+        screens['opponent'].update();
+
+        screens['opponent'].startScroll('left', 0, 15);
+
+        is_set_up = true;
     } catch(e){
-        console.log('There was a problem getting set up:', e);
+        console.log('Error:', e);
     }
 
-    if(!is_setup){
-        return console.log("Can't continue without being set up properly. Exiting.");
+    if(!is_set_up){
+        return console.log('There was a problem setting up.');
     }
+
+    screens['action'].clear()
+        .cursor(0,3).print('Bootlegmon')
+        .cursor(1,2).print('Continue >>>');
     
-    // Begin a game.
-    game();
+    // Wait for the action button to be pressed,
+    // then begin the game.
+    emitter.once('action', function(){
+        startGame();
+    });
 });
 
-function moveObstacle(obstacles){
-    
-    // Obstacles is an array of the obstacles currently on screen,
-    // and as such, we should iterate through each obstacle.
-    for(let i = 0; i < obstacles.length; ++i){
-        
-        // Check if this obstacle has hit the player.
-        if(obstacles[i].coords.join('') === player.join('')){
-            died = true;
-            break;
-        }
-        
-        // Clear the previous position.
-        screen.cursor(...obstacles[i].coords).print(' ');
-        
-        // If the obstacle has met the edge of the screen,
-        // remove it from the list, effectively deleting it.
-        if(!obstacles[i].coords[1]){
-            obstacles.splice(i,1);
-            continue;
-        }
-        
-        obstacles[i].coords[1] -= 1;
-        screen.cursor(...obstacles[i].coords).print(`:${obstacles[i].char}:`);
-    }
+board.on('close', () => {
+    // Avoid prolonged beep.
+    piezo.noTone().off();
+
+    // Clear screens and print goodbye message.
+    screens['action'].clear().home().print('Goodbye');
+    screens['opponent'].clearDisplay();
+    screens['opponent'].update();
+});
+
+function drawOpponent(sprite){
+    screens['opponent'].clearDisplay();
+    screens['opponent'].buffer = sprite;
+    screens['opponent'].invertDisplay(1);
+    screens['opponent'].update();
 }
 
-function generateObstacle(){
-    let r = Math.floor(Math.random() * obstacleChars.length);
-    
-    screen.cursor(1,15).print(`:${obstacleChars[r]}:`);
-    
-    return {
-        coords: [1,15],
-        char: obstacleChars[r]
-    }
+function drawActions(){
+    screens['action'].clear()
+    .home().print('1.Attack 2.Bag')
+    .cursor(1,0).print('3.Run')
+    .home().blink();
 }
 
-// Define the player jump action.
-function jump(){
-    if(!died){
-        isJumping = true;
-        
-        screen.cursor(...player).print(' ');
-        
-        player[0] = 0;
-        screen.cursor(...player).print(':runninga:');
-        
-        setTimeout(() => {
-            screen.cursor(...player).print(' ');
-            
-            player[0] = 1;
-            screen.cursor(...player).print(':runningb:');
-            
-            isJumping = false;
-        }, 1000);
-    }
-    
+function cycleOptions(){
+    currCursor += currCursor === 2 ? -2 : 1;
+    currOption = optionNames[currCursor];
+
+    screens['action'].cursor(...optionCursors[currCursor]).blink();
 }
 
-function restart(){
-    if(died){
-        died = false;
-        game();
-    }
-}
-
-// Store event handlers (a.k.a. game actions, or simply actions).
-const actions = { jump, restart }
-
-function game(){
-    
-    // Initially wipe the screen,
-    // then draw the player.
-    screen.clear().cursor(...player).print(':runningb:');
-    
-    /*
-    
-    Game Variables
-    ——————————————
-    died - if the player has hit an obstacle.
-    obstacles - the obstacles currently on the screen.
-    
-    */
-    const obstacles = [];
-    
-    // Game loop.
-    while(!died){
-        
-        // If there are no obstacles, generate one.
-        // Likewise if the cursor has reached the edge of the screen.
-        if(!obstacles.length){
-            obstacles.push(generateObstacle());
-        }
-        
-        // Otherwise, inch the obstacle ever closer.
-        else {
-            moveObstacle(obstacles);
-        }
-        
-        // Slow the refreshes down--effectively sets a refresh rate.
-        wait.for.time(.25);
-    }
-    
-    screen.clear();
-    gameOver();
+function selectOption(){
+    actions[currOption]();
 }
 
 function gameOver(){
-    screen.clear().home().print('Game Over');
-    
-    wait.for.time(1);
-    
-    screen.clear().home().print('Click the button');
-    screen.cursor(1,0).print('to play again.');
+    for(btn in buttons){
+        // Effectively removes event listener. Can't use
+        // removeListener, since functions aren't named.
+        buttons[btn].on('press', () => {});
+    }
+
+    screens['action'].clear().home().print('Game Over.');
+    play(piezo, anthem);
+}
+
+function startGame(){
+    // Prep screens
+    screens['opponent'].stopScroll();
+
+    drawOpponent(pikachu);
+    drawActions();
+
+    // Events
+    emitter.on('cycle', cycleOptions);
+    emitter.on('action', selectOption);
+
+    emitter.on('game-over', function(){
+        gameOver();
+    });
 }
